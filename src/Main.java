507a9cc5e77aa787614f6cf4fb8f774e5c0dc399
Mihaela C <mchiaburu.1337@gmail.com>
2023-09-23 11:00:06 +0300

    new main

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,11 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+        System.out.println("Hell1!");
+        System.out.println("Hell2!");
+        System.out.println("Hell3!");
+        System.out.println("Hell4!");
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
